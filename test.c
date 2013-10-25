5fdc4ed1941fdd96dcd5bc094fc75f6129a69547
moker <rmokerone@gmail.com>
2013-10-25 21:32:34 +0800

    Add pwm led

--- test.c
+++ test.c
@@ -1,18 +1,24 @@
 #define F_CPU 4.8E6
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
+volatile unsigned char light = 254;
+volatile char mark = 1;
+unsigned char led = 0x0f;
 
 void Init_IO(void);
-void display_led (char num);
-
-unsigned char led = 0x0f;
+void display_led_mode1 (char num); 
+void display_led_mode2 (void);
+void Init_timer ();
 
 int main(void) 
 {
     Init_IO();
+    display_led_mode2 ();
     while (1)
     {
-	display_led (4);
+	//display_led_mode1 (4);
     }
 }
 
@@ -21,7 +27,7 @@ void Init_IO (void)
    DDRB = 0xff;
 }
 
-void display_led (char num)
+void display_led_mode1 (char num)
 {
     char i;
     
@@ -32,3 +38,35 @@ void display_led (char num)
     } 
     led = 0x0f;
 }
+   
+void display_led_mode2 (void)
+{
+    Init_timer();   
+}
+
+void Init_timer ()
+{
+    TCCR0A = 0X03;
+    TCCR0B = 0X03;
+    TCNT0 = 0X00;
+    OCR0A = 0x00;
+    TIMSK0 |= (1<<OCF0A)|(1<<TOIE0);
+    sei();
+}
+
+ISR (TIM0_COMPA_vect)
+{
+    PORTB = 0x00;
+}
+
+ISR (TIM0_OVF_vect)
+{
+    PORTB = 0Xff;
+    if (mark == 1)
+        OCR0A = light++;
+    else
+        OCR0A = light--;
+    if ((light == 0xff) | (light == 0x00))
+        mark =~ mark;
+}
+
